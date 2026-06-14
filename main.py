2f36190ea384ef3bfdb84a723fb8d7270f300fdc
elitamor73-spec <elitamor73@gmail.com>
2026-06-14 10:11:19 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-    from fastapi import FastAPI
 from fastapi.responses import HTMLResponse
 
 app = FastAPI()
@@ -6,4 +5,4 @@ app = FastAPI()
 @app.get("/")
 async def home():
     return HTMLResponse(content="<h1>Server is Running!</h1><p>سلام! اگر این صفحه را میبینید یعنی سرور رندر سالم است.</p>")
-    
+
